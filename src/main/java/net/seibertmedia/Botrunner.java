1fe8a6176e20fbcf148cb6644cfaca2a401ca594
Michael Frankl <mfrankl@seibert-media.net>
2016-06-17 07:57:37 +0200

    Set language to german

--- src/main/java/net/seibertmedia/Botrunner.java
+++ src/main/java/net/seibertmedia/Botrunner.java
@@ -12,44 +12,48 @@ import net.seibertmedia.bots.AliceHandler;
 
 public class Botrunner {
 
-    public static final String BOT_NAME = "bot-name";
+  public static final String BOT_NAME = "bot-name";
 
-    public static final String BOT_PATH = "bot-path";
+  public static final String BOT_PATH = "bot-path";
 
-    public static final String TELEGRAM_BOT_KEY = "telegram-bot-key";
+  public static final String TELEGRAM_BOT_KEY = "telegram-bot-key";
+
+    public static final String LANGUAGE = "language";
+
+    public static final String COUNTRY = "country";
 
     public static void main(String[] args) {
 
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+    TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
 
-        Properties properties = Botpropperties.readProperties();
+    Properties properties = Botpropperties.readProperties();
+    String language = properties.getProperty(LANGUAGE);
+    String country = properties.getProperty(COUNTRY);
+    Locale l = new Locale(language, country);
+    setDefault(l);
 
-        Locale l = new Locale("de", "DE");
-        setDefault(l);
+    checkProperties(properties);
 
-        checkProperties(properties);
-
-        try {
-            telegramBotsApi.registerBot(
-                new AliceHandler(
-                    properties.getProperty(BOT_NAME),
-                    properties.getProperty(BOT_PATH),
-                    properties.getProperty(TELEGRAM_BOT_KEY)
-                ));
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
+    try {
+      telegramBotsApi.registerBot(
+          new AliceHandler(
+              properties.getProperty(BOT_NAME),
+              properties.getProperty(BOT_PATH),
+              properties.getProperty(TELEGRAM_BOT_KEY)));
+    } catch (TelegramApiException e) {
+      e.printStackTrace();
     }
+  }
 
-    private static void checkProperties(final Properties properties) {
-        checkProperty(properties, BOT_NAME);
-        checkProperty(properties, BOT_PATH);
-        checkProperty(properties, TELEGRAM_BOT_KEY);
-    }
+  private static void checkProperties(final Properties properties) {
+    checkProperty(properties, BOT_NAME);
+    checkProperty(properties, BOT_PATH);
+    checkProperty(properties, TELEGRAM_BOT_KEY);
+  }
 
-    private static void checkProperty(final Properties properties, final String parameterName) {
-        if(properties.getProperty(parameterName)==null || properties.getProperty(parameterName).isEmpty()) {
-            throw new RuntimeException("Parameter missing: " + parameterName + ". Please define it in settings.properties");
-        }
+  private static void checkProperty(final Properties properties, final String parameterName) {
+    if (properties.getProperty(parameterName) == null || properties.getProperty(parameterName).isEmpty()) {
+      throw new RuntimeException("Parameter missing: " + parameterName + ". Please define it in settings.properties");
     }
+  }
 }
