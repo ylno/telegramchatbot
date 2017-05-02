fe96f353eca9b21d1840cbc0784cba970aaca445
mfrankl <mfrankl@seibert-media.net>
2017-05-02 10:11:19 +0200

    News not as html

--- src/main/java/net/seibertmedia/bots/AliceHandler.java
+++ src/main/java/net/seibertmedia/bots/AliceHandler.java
@@ -96,9 +96,6 @@ public class AliceHandler extends TelegramLongPollingBot {
     logger.info("request from {}: {}", message.getChatId(), message.getText());
 
     String answer = chat.multisentenceRespond(message.getText());
-    if (answer.contains("<")) {
-      sendMessage.enableHtml(true);
-    }
     logger.info("answer to {}: {}", message.getChatId(), answer);
     sendMessage.setText(answer);
     try {
